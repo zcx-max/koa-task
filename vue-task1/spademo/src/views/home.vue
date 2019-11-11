f9ed7bbbb0aa041ba3af225e2a8a5f0671baffe5
zhu <2992815224@qq.com>
2019-11-11 20:31:52 +0800

    前端连接后台

---
+++ vue-task1/spademo/src/views/home.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        首页
+        <button @click="btn">点击获取</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                
+            }
+        },
+        methods: {
+            btn(){
+                this.axios.get('/api/list').then(res=>{
+                    console.log(res)
+                })
+            }
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
